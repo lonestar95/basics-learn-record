df4911081fe955ebd81c8a6cf98f108b536daa36
yilostar <121923077+lonestar95@users.noreply.github.com>
2023-01-06 21:59:53 +0800

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+main()
+
+
+
+
+
+
+
+//{
+	int s,t,A=10;//int 4λ
+	double B=6;//double 8λ
+	s=sizeof(A);
+	t=sizeof(B);
+	printf("%d,%d\n",s,t);
+}
+
+
+
+
+//{int a,b,sum;
+a=10;
+b=20;
+sum=a+b;
+printf("a=%d,b=%d,sum=%d\n",a,b,sum);
+}
+
+//#include <stdio.h>
+main()
+{
+	printf("***************\n");
+  printf("    hello!    \n ");
+	printf("***************");
+}
